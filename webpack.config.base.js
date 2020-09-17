2dab6cf6113cf7b04a6168c80ff8907dc25a9fc7
Xinjn <422075616@qq.com>
2020-09-17 23:46:48 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,4 +14,28 @@ module.exports = {
             template: 'src/assets/index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                loader: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    // 将 JS 字符串生成为 style 节点
+                    'style-loader',
+                    // 将 CSS 转化成 CommonJS 模块
+                    'css-loader',
+                    // 将 Sass 编译成 CSS
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    }
+                ],
+            },
+        ],
+    },
 };
