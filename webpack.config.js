b7a07ce77075f5ddbdda83dbc8cda0269d26b9e0
Xinjn <422075616@qq.com>
2020-09-17 21:52:14 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
-    }
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
+
+
 
